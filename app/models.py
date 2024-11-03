6f9b75a59e76c94a77c6525e8f94bfc9a767125a
TheoCoelho <118085127+TheoCoelho@users.noreply.github.com>
2024-11-03 20:06:57 -0300

    inicio gestao de projetos

--- app/models.py
+++ app/models.py
@@ -8,3 +8,23 @@ class Service(db.Model):
 
     def __repr__(self):
         return f"<Service {self.name}>"
+
+
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    columns = db.relationship('Column', backref='board', lazy=True)
+
+class Column(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    position = db.Column(db.Integer)  # Para ordenar as colunas no front-end
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'), nullable=False)
+    tasks = db.relationship('Task', backref='column', lazy=True)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+    position = db.Column(db.Integer)  # Para ordenar as tarefas na coluna
+    column_id = db.Column(db.Integer, db.ForeignKey('column.id'), nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -26,3 +26,30 @@ def pricing():
 @main.route('/testimonials')
 def testimonials():
     return render_template('testimonials.html')
+
+@main.route('/gerenciamentodeprojetos')
+def  gerenciamentodeprojetos():
+    return render_template('gerenciamentodeprojetos.html')
+
+
+
+@main.route('/boards/<int:board_id>/columns', methods=['POST'])
+def add_column(board_id):
+    column_name = request.form['name']
+    new_column = Column(name=column_name, board_id=board_id, position=0)  # Defina a posição corretamente
+    db.session.add(new_column)
+    db.session.commit()
+    return redirect(url_for('main.board', board_id=board_id))
+
+@main.route('/tasks/move', methods=['POST'])
+def move_task():
+    task_id = request.form['task_id']
+    new_column_id = request.form['column_id']
+    new_position = request.form['position']
+
+    task = Task.query.get(task_id)
+    task.column_id = new_column_id
+    task.position = new_position
+    db.session.commit()
+
+    return {'status': 'success'}
